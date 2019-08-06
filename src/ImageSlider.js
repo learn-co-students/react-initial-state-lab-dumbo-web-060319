24b60b28760a9f41fd48c3b0122e3cb42c64a998
Alexander Schelchere <ascellie@gmail.com>
2019-08-06 10:52:15 -0400

    Image Slider

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 class ImageSlider extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  
   render() {
-    return <div>Image Slider</div>
+    return <div>I am on slide {this.state.currentSlideIndex}</div>
   }
 }
 
